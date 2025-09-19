a3b3c14c9b2c14121de8e3bc23d68ceb2218fff7
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2025-09-19 23:41:22 +0530

    update run.sh and docker-compose.yml to use consistent HOST_IP for environment variables

--- self-hosted/run.sh
+++ self-hosted/run.sh
@@ -6,7 +6,7 @@ set -e
 BACKEND_PID=$!
 
 # Wait for the backend to start
-while ! curl -f http://127.0.0.1:3003/instance_name >/dev/null 2>&1; do
+while ! curl -f http://192.168.1.5:3003/instance_name >/dev/null 2>&1; do
     sleep .1
 done
 
@@ -14,7 +14,7 @@ ADMIN_KEY=$(./generate_admin_key.sh)
 
 # Make a .env.local file with the admin key
 echo "CONVEX_SELF_HOSTED_ADMIN_KEY=$ADMIN_KEY" >>.env.local
-echo "CONVEX_SELF_HOSTED_URL=http://127.0.0.1:3003" >>.env.local
+echo "CONVEX_SELF_HOSTED_URL=http://192.168.1.5:3003" >>.env.local
 
 # Deploy the current code to the backend
 npx convex self-host dev --until-success
